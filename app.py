02ee423ce0f707abd641fb9cb0c79beed4210f0b
Pns8787 <prathemprathem30@gmail.com>
2025-04-10 08:44:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,6 +50,6 @@ def index():
     return render_template('index.html')  # Now properly imported
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 4000))
     app.run(host='0.0.0.0', port=port)
     
